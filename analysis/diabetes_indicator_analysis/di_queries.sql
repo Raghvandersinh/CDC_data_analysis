c1a1e42c957445b321701690df07ca6942f3e6bd
Raghvandersinh <raghavndersolanki2@gmail.com>
2026-08-21 15:24:12 -0400

    Added Numer_in_discharges to the query.

--- analysis/diabetes_indicator_analysis/di_queries.sql
+++ analysis/diabetes_indicator_analysis/di_queries.sql
@@ -1,11 +1,17 @@
 COPY(
-    SELECT year, indicator, topic, population, age, race, sex, education, other_info,
+    SELECT unit, year, indicator, topic, population, age, race, sex, education, other_info,
     CASE
         WHEN unit = 'Number in 1,000,000s' THEN estimate * 1000000
         WHEN unit = 'Number in 1,000s' THEN estimate * 1000
+        WHEN unit = 'Number of Discharges in 1,000s' THEN estimate * 1000
         ELSE estimate
-    END as true_number_estimate
+    END as true_number_estimate,
+    CASE
+        WHEN unit LIKE '%Number%' and unit Like '%Discharges%' THEN 'Discharge'
+        ELSE 'Normal'
+    END AS unit_category
     FROM di_db.diabetes_ind
     WHERE unit LIKE '%Number%' and estimate is not NULL
     ORDER BY unit ASC
 ) TO 'true_number_estimate.csv' (HEADER, DELIMITER ',');
+
