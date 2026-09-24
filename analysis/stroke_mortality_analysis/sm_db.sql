80e8252de4f65d0c318eca2a33b72643c1c560c7
Raghvandersinh <raghavndersolanki2@gmail.com>
2026-09-24 14:51:52 -0400

    Added sm_db.duckdb to gitignore.
    cleaned sex, race, and location data.

---
+++ analysis/stroke_mortality_analysis/sm_db.sql
@@ -0,0 +1,16 @@
+
+DESCRIBE sm_db.stroke_mortality;
+
+SELECT DISTINCT race FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT sex FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT location, geo_level FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT state FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT icd_class FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT geo_level FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT topic FROM sm_db.stroke_mortality;
