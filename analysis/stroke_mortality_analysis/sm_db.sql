efd336d2cf5df234aef3ed5a9df2aaaff434bdd9
Raghvandersinh <raghavndersolanki2@gmail.com>
2026-09-25 14:45:22 -0400

    Removed null values, created a new dataframe that focuses on NJ only with overall sex and race. created quries for data exploration
    installed statsmodels. we are going to do some time series forecast analysis.

--- analysis/stroke_mortality_analysis/sm_db.sql
+++ analysis/stroke_mortality_analysis/sm_db.sql
@@ -1,4 +1,3 @@
-
 DESCRIBE sm_db.stroke_mortality;
 
 SELECT DISTINCT race FROM sm_db.stroke_mortality;
@@ -14,3 +13,8 @@ SELECT DISTINCT icd_class FROM sm_db.stroke_mortality;
 SELECT DISTINCT geo_level FROM sm_db.stroke_mortality;
 
 SELECT DISTINCT topic FROM sm_db.stroke_mortality;
+
+SELECT DISTINCT state FROM sm_db.stroke_mortality;
+
+SELECT COUNT(state) FROM sm_db.stroke_mortality
+where state = 'NJ';
